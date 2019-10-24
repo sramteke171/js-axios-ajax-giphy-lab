4d3a3461ced5df5f229509f2c0e6f35b97af3570
Marc Wright <marc.wright@gmail.com>
2019-10-24 09:58:40 -0400

    Refactor into one giphy axios call

--- solution.js
+++ solution.js
@@ -1,71 +1,37 @@
 const giphyRandomEndpoint = `https://api.giphy.com/v1/gifs/random?api_key=2041494ca782403cb6055682a7943c75&tag=&rating=G`
 const giphyTrendingEndpoint = `https://api.giphy.com/v1/gifs/trending?api_key=2041494ca782403cb6055682a7943c75&tag=&rating=G`
 
-// GRAB THE BUTTON ELEMENT FROM THE DOM
-let getRandomGiphyButton = document.querySelector('#getRandomGiphyButton');
-let getTrendingGiphyButton = document.querySelector('#getTrendingGiphyButton');
+let giphyImageTag = document.querySelector('#giphyImageTag')
+let giphyTitleDiv = document.querySelector('#giphyTitleDiv')
 
+// GRAB THE BUTTON ELEMENT FROM THE DOM AND ADD EVENT LISTENERS
+let getRandomGiphyButton = document.querySelector('#getRandomGiphyButton');
 getRandomGiphyButton.addEventListener('click', axiosGiphy)
+
+let getTrendingGiphyButton = document.querySelector('#getTrendingGiphyButton');
 getTrendingGiphyButton.addEventListener('click', axiosGiphy)
 
 function axiosGiphy(event) {
+  // WE SET WHICH ENDPOINT TO HIT BASED ON THE ID OF THE CLICKED BUTTON
   let axiosUrl = event.target.id === 'getRandomGiphyButton' ? giphyRandomEndpoint : giphyTrendingEndpoint;
-  console.log(event.target)
+
   axios({
     url: axiosUrl,
     method: 'get'
   })
     .then(response => {
-      let embededImageUrl = ''
-      let giphyTitle = ''
-      console.log(event.target)
-      if (event.target.id === 'getRandomGiphyButton') {
-        embededImageUrl = response.data.data.image_url
-        giphyTitle = response.data.data.title
-      } else {
-        embededImageUrl = response.data.data[0].images.downsized.url
-        giphyTitle = response.data.data[0].title
-      }
-      console.log(response)
       // IN THE .THEN METHOD, CONSOLE.LOG THE JSON RESPONSE OBJECT  
-      // AND FIND THE image_url
-
+      // AND FIND THE image_url and title
+      let trendingGifIndex = Math.floor(Math.random() * 25);
+      let giphyData = response.data.data
+      let embededImageUrl = giphyData[0] ? giphyData[trendingGifIndex].images.downsized.url : giphyData.image_url;
+      let giphyTitle = giphyData[0] ? giphyData[trendingGifIndex].title : giphyData.title;
 
       // CHANGE THE SRC ATTRIBUTE ON THE IMG TAG TO THE image_url
-      let giphyImageTag = document.querySelector('#giphyImageTag')
       giphyImageTag.setAttribute('src', embededImageUrl)
 
       // CHANGE THE INNER TEXT OF THE giphyTitleDiv TO  THE title KEY
-      let giphyTitleDiv = document.querySelector('#giphyTitleDiv')
       giphyTitleDiv.innerText = giphyTitle
     })
     .catch(error => console.log(error))
 }
-
-// // ADD A CLICK EVENT LISTENER
-// getRandomGiphyButton.addEventListener('click', function () {
-//   // INSIDE THE EVENT HANDLER, MAKE AN AXIOS GET REQUEST TO THE 
-//   // giphyRandomEndpoint ENDPOINT
-//   axios({
-//     url: giphyRandomEndpoint,
-//     method: 'get'
-//   })
-//     .then(response => {
-//       // IN THE .THEN METHOD, CONSOLE.LOG THE JSON RESPONSE OBJECT  
-//       // AND FIND THE image_url
-//       let embedUrl = response.data.data.image_url
-//       let giphyTitle = response.data.data.title
-//       console.log(response.data)
-
-//       // CHANGE THE SRC ATTRIBUTE ON THE IMG TAG TO THE image_url
-//       let giphyImageTag = document.querySelector('#giphyImageTag')
-//       giphyImageTag.setAttribute('src', embedUrl)
-
-//       // CHANGE THE INNER TEXT OF THE giphyTitleDiv TO  THE title KEY
-//       let giphyTitleDiv = document.querySelector('#giphyTitleDiv')
-//       giphyTitleDiv.innerText = giphyTitle
-//     })
-//     .catch(error => console.log(error))
-
-// })
-
