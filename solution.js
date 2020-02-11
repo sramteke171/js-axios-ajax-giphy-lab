e046d0b8b8ead9663e9cb0cd7073282998fcd019
Marc Wright <marc@generalassemb.ly>
2020-02-11 13:48:03 -0600

    Add search box solution

--- solution.js
+++ solution.js
@@ -1,5 +1,7 @@
-const giphyRandomEndpoint = `https://api.giphy.com/v1/gifs/random?api_key=2041494ca782403cb6055682a7943c75&tag=&rating=G`
-const giphyTrendingEndpoint = `https://api.giphy.com/v1/gifs/trending?api_key=2041494ca782403cb6055682a7943c75&tag=&rating=G`
+const api_key = '2041494ca782403cb6055682a7943c75'
+
+const giphyRandomEndpoint = `https://api.giphy.com/v1/gifs/random?api_key=${api_key}&tag=&rating=G`
+const giphyTrendingEndpoint = `https://api.giphy.com/v1/gifs/trending?api_key=${api_key}&tag=&rating=G`
 
 let giphyImageTag = document.querySelector('#giphyImageTag')
 let giphyTitleDiv = document.querySelector('#giphyTitleDiv')
@@ -10,6 +12,39 @@ getRandomGiphyButton.addEventListener('click', axiosGiphy)
 
 let getTrendingGiphyButton = document.querySelector('#getTrendingGiphyButton');
 getTrendingGiphyButton.addEventListener('click', axiosGiphy)
+
+let getSearchGiphyButton = document.querySelector('#getSearchGiphyButton');
+getSearchGiphyButton.addEventListener('click', searchGiphy)
+
+function searchGiphy() {
+  let userInput = document.querySelector("input[name='user-input']").value
+  let giphySearchEndpoint = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${userInput}`
+
+  axios({
+    url: giphySearchEndpoint,
+    method: 'GET'
+  })
+  .then(response => {
+    let searchGifsDiv = document.querySelector('#searchGifs')
+    let searchGifsArray = response.data.data
+
+    // let gifsImageArray = searchGifsArray.map(gif => {
+    //   let newGif = document.createElement('img')
+    //   newGif.setAttribute('src', gif.images.original.url)
+    //   return newGif
+    // }) 
+    
+    // console.log(gifsImageArray)
+    // searchGifsDiv.append(gifsImageArray)
+
+    searchGifsArray.forEach(gif => {
+      let newGif = document.createElement('img')
+      newGif.setAttribute('src', gif.images.original.url)
+      searchGifsDiv.appendChild(newGif)
+    })    
+  })
+}
+
 
 function axiosGiphy(event) {
   // WE SET WHICH ENDPOINT TO HIT BASED ON THE ID OF THE CLICKED BUTTON
