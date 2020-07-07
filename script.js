4721d5af742dcc6d9a404f1c0da5d3ff248cc608
Leo <leonardo@Leo.home>
2020-07-07 13:48:42 -0400

    starter code

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const giphyRandomEndpoint = `https://api.giphy.com/v1/gifs/random?api_key=2041494ca782403cb6055682a7943c75&tag=&rating=G`
-
+const giphyRandomEndpoint = `https://api.giphy.com/v1/gifs/random?api_key=2041494ca782403cb6055682a7943c75&tag=&rating=G`;
 
 // GRAB THE BUTTON ELEMENT FROM THE DOM
 
 // ADD A CLICK EVENT LISTENER
 
-// INSIDE THE EVENT HANDLER, MAKE AN AXIOS GET REQUEST TO THE 
+// INSIDE THE EVENT HANDLER, MAKE AN AXIOS GET REQUEST TO THE
 // giphyRandomApiUrl ENDPOINT
 
-// IN THE .THEN METHOD, PARSE THE JSON RESPONSE OBJECT AND FIND 
+// IN THE .THEN METHOD, PARSE THE JSON RESPONSE OBJECT AND FIND
 // THE image_url key
 
 // CHANGE THE SRC ATTRIBUTE ON THE IMG TAG TO THE image_url KEY
